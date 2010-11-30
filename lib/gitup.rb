5f15342cf28aeecf97999857b0c388a78f2b0df6
David Svensson <david@standout.se>
2010-11-30 22:41:40 +0100

    Version 0.0.1

--- lib/gitup.rb
+++ lib/gitup.rb
@@ -1,22 +1,62 @@
+
 class Gitup
   
-  def gitup!
-    puts "Inside gitup."
-    puts current_branch
+  attr_accessor :stashing
+  
+  def self.gitdown!
+    
+    unless everything_commited?
+      system "git status"
+      puts "You have uncommitted changes in #{current_branch}. Do you want to try gitdown anyway? (y/n)"
+      input = gets.chomp
+      if input.downcase == 'y'
+        system "git stash"
+        self.run_gitdown_commands(current_branch)
+        system "git stash apply"
+      else
+        puts "Ok... coward."
+      end
+    else
+      self.run_gitdown_commands(current_branch)
+    end
+  end
+  
+  def self.gitup!
+    
     unless everything_commited?
       system "git status"
       raise "Hold your horses! You must commit your changes first."
     end
     
+    # Push it!
+    self.run_gitup_commands
+    
+    puts "Finished."
+    
   end
   
-  def current_branch
-    branch = `git branch`
-    puts "Branch: #{branch}"
+  def self.current_branch
+    return `git branch`.chomp.gsub('* ', '').gsub("\n", '')
   end
 
   # Do we have anything uncommitted in this repository?
-  def everything_commited?
+  def self.everything_commited?
     `git ls-files --deleted --modified --others --exclude-standard` == ""
   end
+  
+  def run_gitup_commands(branch)
+    system "git checkout master"
+    system "git merge #{branch}"
+    system "git push origin master"
+    system "git checkout #{branch}"
+    system "git rebase master"
+  end
+  
+  def run_gitdown_commands(branch)
+    system "git checkout master"
+    system "git pull origin master"
+    system "git checkout #{branch}"
+    system "git rebase master"
+  end
+  
 end
